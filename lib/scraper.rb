8920b9a3e876e2e78a2d404f86d41f503dd6d9a3
Morgan Matney <morgan.matney89@gmail.com>
2020-09-14 17:37:09 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,7 @@ require_relative './course.rb'
 class Scraper
 
   def get_page
-    doc = Nokokiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end
 
   def print_courses
